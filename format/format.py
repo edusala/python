a2cd711eb7bab242d4737a21444069150ca34f86
Sallai András <valaki@valahol.hu>
2020-09-17 19:47:47 +0200

    Formázott kivitel Matematikai kifejezések Véletlenszám Operátorok Konvertálás Póker

---
+++ format/format.py
@@ -0,0 +1,17 @@
+
+szam = 35.123
+print("|%-20.2f|" % szam)
+
+szam2 = 3425.1234
+print("|%-20.2f|" % szam2)
+
+nev = 'Sallai András'
+print("|%20s|" % nev)
+
+ar = 340480.005
+print("Ár: %.2f Ft" % ar)
+
+betu = "a"
+print("%c" % betu)
+
+
